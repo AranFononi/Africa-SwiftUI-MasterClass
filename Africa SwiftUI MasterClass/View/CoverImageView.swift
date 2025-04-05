e4b7433fb0ae30e1cc107cb573a56883954c2aa8
Aran Fononi <aaronfononi@gmail.com>
2025-04-05 17:09:39 +0330

    create CoverImageView

---
+++ Africa SwiftUI MasterClass/View/CoverImageView.swift
@@ -0,0 +1,20 @@
+//
+//  CoverImageView.swift
+//  Africa SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 5/4/25.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - Properties
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoverImageView()
+}
