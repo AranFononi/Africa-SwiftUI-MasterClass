cc295fcb7b102f4675b8553f18ebde2f051b29a2
Aran Fononi <aaronfononi@gmail.com>
2025-04-05 13:22:26 +0330

    Initial Commit

---
+++ Africa SwiftUI MasterClass/Africa_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_SwiftUI_MasterClassApp.swift
+//  Africa SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Africa_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
