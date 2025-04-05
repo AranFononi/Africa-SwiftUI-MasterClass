f60ebfc1e6825dda8c65278d8296052beda9dc2d
Aran Fononi <aaronfononi@gmail.com>
2025-04-05 13:44:14 +0330

    Create MainView

---
+++ Africa SwiftUI MasterClass/App/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Africa SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 5/4/25.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
