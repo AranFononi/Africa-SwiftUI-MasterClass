eeda7af019d8d5b0fe9f49402c1121550a415443
Aran Fononi <aaronfononi@gmail.com>
2025-04-05 21:16:34 +0330

    Create MapModel

---
+++ Africa SwiftUI MasterClass/Model/MapModel.swift
@@ -0,0 +1,21 @@
+//
+//  MapModel.swift
+//  Africa SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 5/4/25.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
