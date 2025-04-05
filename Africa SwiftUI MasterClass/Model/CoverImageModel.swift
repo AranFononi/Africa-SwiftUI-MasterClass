a761b8253a9d55f3d60c2b2e78ae65c353ebe86b
Aran Fononi <aaronfononi@gmail.com>
2025-04-05 17:17:18 +0330

    Create CoverImageModel for Cover JSON

---
+++ Africa SwiftUI MasterClass/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 5/4/25.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id:Int
+    let name: String
+}
