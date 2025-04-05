58e87740fee70747adec2f4bf5b0d1e12d34b5ce
Aran Fononi <aaronfononi@gmail.com>
2025-04-05 20:05:24 +0330

    Create VideoModel

---
+++ Africa SwiftUI MasterClass/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 5/4/25.
+//
+
+import SwiftUI
+
+struct Video: Codable, Identifiable {
+    let id : String
+    let title : String
+    let headline : String
+}
